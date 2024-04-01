f482f14d47540f334b8f8b52591a3b18bd1e5efa
Rajesh Soni <rajeshsoni0129@gmail.com>
2024-04-01 23:46:11 +0530

    Updates

---
+++ precache-manifest.66811eeca8adb227aee1cd4ca08e216c.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dcacced44e695f7b1deff0c50582651",
+    "url": "/NewsApp/index.html"
+  },
+  {
+    "revision": "3faf4186dc30ae7cd754",
+    "url": "/NewsApp/static/css/2.75bc9036.chunk.css"
+  },
+  {
+    "revision": "92fcbfe001ee3732b817",
+    "url": "/NewsApp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "3faf4186dc30ae7cd754",
+    "url": "/NewsApp/static/js/2.3fab892f.chunk.js"
+  },
+  {
+    "revision": "cc1e1b6d0da3b64d4e1500cf92efedf2",
+    "url": "/NewsApp/static/js/2.3fab892f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7436073740f30f28c064",
+    "url": "/NewsApp/static/js/3.de4c2f94.chunk.js"
+  },
+  {
+    "revision": "92fcbfe001ee3732b817",
+    "url": "/NewsApp/static/js/main.ddd7fb2c.chunk.js"
+  },
+  {
+    "revision": "62d35654300ec8ee91bb",
+    "url": "/NewsApp/static/js/runtime-main.01c0a7f9.js"
+  },
+  {
+    "revision": "a56effccdb31215be2405af923623ecc",
+    "url": "/NewsApp/static/media/1.a56effcc.jpg"
+  }
+]);
